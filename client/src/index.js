45bdcdd22547ae26c21e874bdb05b4f23c3fc60c
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2023-01-14 14:05:51 -0700

    more auth

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { initMiddleware } from 'devise-axios';
+import AuthProvider from './providers/AuthProvider';
+
+initMiddleware();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <AuthProvider>
     <App />
+    </AuthProvider>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
